0af497feed5ef7e7b1c603b70b071a9b1b8dba12
zuoshifan <zuoshifan@163.com>
2016-08-19 16:47:39 +0800

    Clean in r_pca

--- r_pca.py
+++ r_pca.py
@@ -1,4 +1,3 @@
-# from __future__ import division
 import numpy as np
 from scipy import linalg as la
 
@@ -15,16 +14,11 @@ class R_pca(object):
     def __init__(self, M, mu=None, lmbda=None, verbose=True):
         self.M = M
         self.verbose = verbose
-        # self.S = np.zeros_like(M)
-        # self.Y = np.zeros_like(M)
 
         if mu is not None:
             self.mu = mu
         else:
-            # self.mu = np.prod(M.shape) / (4 * self.norm_p(M, 2))
             self.mu = np.prod(M.shape) / (4.0 * la.norm(M, ord=1))
-
-        # self.mu_inv = 1.0 / self.mu
 
         if lmbda is not None:
             self.lmbda = lmbda
@@ -35,10 +29,6 @@ class R_pca(object):
             print 'mu    = ', self.mu
             print 'lmbda = ', self.lmbda
 
-    # @staticmethod
-    # def norm_p(M, p):
-    #     return np.sum(np.power(M, p))
-
     @staticmethod
     def shrink(M, tau):
         return np.sign(M) * np.maximum(np.abs(M) - tau, 0.0)
@@ -46,7 +36,6 @@ class R_pca(object):
     @staticmethod
     def svd_threshold(M, tau):
         U, s, VT = la.svd(M, full_matrices=False)
-        # return np.dot(U*self.shrink(s, tau), VT)
         return np.dot(U*R_pca.shrink(s, tau), VT)
 
     def fit(self, tol=1.0e-7, max_iter=1000, iter_print=100, return_err=False):
@@ -55,16 +44,7 @@ class R_pca(object):
         mu_inv = 1.0 / self.mu
         Sk = np.zeros_like(self.M)
         Yk = np.zeros_like(self.M)
-        # Lk = np.zeros(self.D.shape)
 
-        # if tol is None:
-        #     tol = 1.0e-7
-        # if tol:
-        #     _tol = tol
-        # else:
-        #     _tol = 1E-7 * self.norm_p(np.abs(self.D), 2)
-
-        # err = np.Inf
         MF = la.norm(self.M, ord='fro')
 
         while cnt < max_iter:
@@ -90,16 +70,3 @@ class R_pca(object):
             return Lk, Sk, err
         else:
             return Lk, Sk
-
-        # while (err > tol and cnt < max_iter):
-        #     mYk = mu_inv*Yk
-        #     Lk = self.svd_threshold(self.M - Sk + mYk, mu_inv)
-        #     Sk = self.shrink(self.M - Lk + mYk, self.lmbda*mu_inv)
-        #     Yk = Yk + self.mu * (self.M - Lk - Sk)
-
-        #     err = la.norm(self.M - Lk - Sk, ord='fro') / MF
-        #     cnt += 1
-        #     if (cnt % cnt_print) == 0 or cnt == 1 or cnt > max_iter or err <= _tol:
-        #         print 'cntation: {0}, error: {1}'.format(cnt, err)
-
-        # return Lk, Sk
